17ec0c863bb4323724b09ad77bb57e273bfacc9e
Hadi <dev@warungcoding.tv>
2026-07-31 00:47:44 +0000

    feat: theme gallery section (data-driven, siap disambungkan ke tabel Template)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import WhyUs from '@/components/WhyUs';
 import HowItWorks from '@/components/HowItWorks';
+import ThemeGallery from '@/components/ThemeGallery';
 
 export default function Home() {
   return (
@@ -10,8 +11,9 @@ export default function Home() {
       <main>
         <Hero />
         <WhyUs />
+        <ThemeGallery />
         <HowItWorks />
-        {/* Section berikutnya (Tema, Fitur detail, Harga, dst) menyusul di task selanjutnya */}
+        {/* Section berikutnya (Fitur detail, Harga, dst) menyusul di task selanjutnya */}
       </main>
     </>
   );
